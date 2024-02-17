5951d23a1e8948f628480f9f6d1f4a77e6a1ee25
Radha <radhasharma2786@gmail.com>
2024-02-17 14:29:49 +0530

    multer~setup

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,16 @@
+import multer from "multer";
+
+// here we are using disk storage (not memory storage kyuki agr files badi hui to memory jyada bharegi consumption jyada hoga which is bad)
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // console.log(file);
+      cb(null, "./public/temp");// saari files public folder k andar rakhenge
+    },
+    // koi error nahi hai that's why first parameter is null
+    
+    filename: function (req, file, cb) {
+      cb(null, file.originalname) // hum original name se save kra rhe hain
+    }
+  })
+  
+  export const upload = multer({ storage })
